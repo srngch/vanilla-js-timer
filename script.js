c431ffefd462f8408c4424e2ae6c55802411dc78
srngch <srngch.dev@gmail.com>
2021-09-21 02:58:58 +0900

    apply eslint

--- script.js
+++ script.js
@@ -1,136 +1,200 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-  App();
-});
-
 const $ = (selector) => document.querySelector(selector);
 
-function App() {
-  let myTimer = {
-    inputSeconds: undefined,
-    leftSeconds: undefined,
-    id: undefined,
-    state: "Start",
-  };
+function convertTimeToSeconds(hours, minutes, seconds) {
+  return hours * 3600 + minutes * 60 + seconds;
+}
 
-  const formTime = document.timeForm;
-  const divDisplayHour = $(".display_hour");
-  const divDisplayMin = $(".display_min");
-  const divDisplaySec = $(".display_sec");
-  const buttonStartPauseResume = $("#startPauseResume");
-  const buttonReset = $("#reset");
-  const divTimeBar = $("#timeBar-left");
+function convertSecondsToTime(totalSeconds) {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+  const seconds = Math.floor(totalSeconds - hours * 3600 - minutes * 60);
+  return { hours, minutes, seconds };
+}
 
-  function timer(seconds) {
+class Timer {
+  constructor(
+    time = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    },
+    option = {
+      tickCallback() {},
+    }
+  ) {
+    this._inputTime = time;
+    this._inputTotalSeconds = convertTimeToSeconds(
+      time.hours,
+      time.minutes,
+      time.seconds
+    );
+    this._state = {
+      id: undefined,
+      remainingSeconds: this._inputTotalSeconds,
+      status: 'Start',
+    };
+    this.tickCallback = option.tickCallback;
+  }
+
+  _go(seconds) {
     const timeStart = Date.now();
     const timeEnd = timeStart + seconds * 1000;
 
-    myTimer.id = setInterval(() => {
+    this._state.id = setInterval(() => {
       const timeLeft = timeEnd - Date.now();
-      myTimer.leftSeconds = Math.round(timeLeft / 1000);
+      this._state.remainingSeconds = Math.round(timeLeft / 1000);
       if (timeLeft < 0) {
-        clearTimer(myTimer.id);
-        myTimer.leftSeconds = 0;
+        this._state.remainingSeconds = 0;
+        this._state.status = 'Finish';
+        this._clear(this._state.id);
         return;
       }
-      displayTime(myTimer.leftSeconds, myTimer.inputSeconds);
+      this.tickCallback(this._state.remainingSeconds, this._inputTotalSeconds);
     }, 1000);
   }
 
-  function startTimer(seconds) {
-    clearInterval(myTimer.id);
-    timer(seconds);
+  get inputTime() {
+    return this._inputTime;
   }
 
-  function pauseTimer(timerId) {
-    clearInterval(timerId);
-  }
-
-  function resumeTimer(seconds) {
-    timer(seconds);
-  }
-
-  function resetTimer() {
-    myTimer.leftSeconds = myTimer.inputSeconds;
-    clearTimer(myTimer.id);
-  }
-
-  function clearTimer(timerId) {
-    clearInterval(timerId);
-    myTimer.state = "Start";
-    buttonStartPauseResume.textContent = "시작";
-    displayTime(myTimer.leftSeconds, myTimer.inputSeconds);
-  }
-
-  function displayTime(leftSeconds, inputSeconds) {
-    const remaining_hour = Math.floor(leftSeconds / 3600);
-    const remaining_min = Math.floor(
-      (leftSeconds - remaining_hour * 3600) / 60
+  set inputTime(time = { hours: 0, minutes: 0, seconds: 0 }) {
+    this._inputTime = time;
+    this._inputTotalSeconds = convertTimeToSeconds(
+      time.hours,
+      time.minutes,
+      time.seconds
     );
-    const remaining_sec = Math.floor(
-      leftSeconds - remaining_hour * 3600 - remaining_min * 60
-    );
-    divDisplayHour.textContent = String(remaining_hour).padStart(2, "0");
-    divDisplayMin.textContent = String(remaining_min).padStart(2, "0");
-    divDisplaySec.textContent = String(remaining_sec).padStart(2, "0");
-    divTimeBar.style.width = `${(leftSeconds * 100) / inputSeconds}%`;
   }
 
-  function inputTime(event) {
-    event.preventDefault();
-    if (myTimer.id !== undefined) clearInterval(myTimer.id);
-    myTimer.state = "Start";
-    buttonStartPauseResume.textContent = "시작";
+  get status() {
+    return this._state.status;
+  }
+
+  start(time = this._inputTime) {
+    clearInterval(this._state.id);
+    this._state.status = 'Pause';
+    this._go(this._inputTotalSeconds);
+  }
+
+  pause() {
+    clearInterval(this._state.id);
+    this._state.status = 'Resume';
+  }
+
+  resume() {
+    this._go(this._state.remainingSeconds);
+    this._state.status = 'Pause';
+  }
+
+  reset() {
+    clearInterval(this._state.id);
+    this._state.remainingSeconds = this._inputTotalSeconds;
+    this._state.status = 'Start';
+    this.tickCallback(this._state.remainingSeconds, this._inputTotalSeconds);
+  }
+
+  _clear() {
+    clearInterval(this._state.id);
+    this._state.status = 'Start';
+    this.tickCallback(this._state.remainingSeconds, this._inputTotalSeconds);
+  }
+}
+
+function App() {
+  const timer = new Timer();
+  timer.tickCallback = tickCallback;
+  const formTime = document.timeForm;
+  const divDisplayHour = $('.display_hour');
+  const divDisplayMin = $('.display_min');
+  const divDisplaySec = $('.display_sec');
+  const buttonStartPauseResume = $('#startPauseResume');
+  const buttonReset = $('#reset');
+  const divTimeBar = $('#timeBar-left');
+
+  function tickCallback(remainingSeconds, totalSeconds) {
+    const remainingTime = convertSecondsToTime(remainingSeconds);
+
+    displayTimeText(
+      remainingTime.hours,
+      remainingTime.minutes,
+      remainingTime.seconds
+    );
+    displayTimeBar(remainingSeconds, totalSeconds);
+  }
+
+  function displayTimeText(hours, minutes, seconds) {
+    divDisplayHour.textContent = String(hours).padStart(2, '0');
+    divDisplayMin.textContent = String(minutes).padStart(2, '0');
+    divDisplaySec.textContent = String(seconds).padStart(2, '0');
+  }
+
+  function displayTimeBar(remainingSeconds, totalSeconds) {
+    divTimeBar.style.width = `${(remainingSeconds * 100) / totalSeconds}%`;
+  }
+
+  function setInputTime() {
     const hours = Number(formTime.hours.value);
     const minutes = Number(formTime.minutes.value);
     const seconds = Number(formTime.seconds.value);
 
-    myTimer.inputSeconds = hours * 3600 + minutes * 60 + seconds;
-    myTimer.leftSeconds = myTimer.inputSeconds;
-    displayTime(myTimer.leftSeconds, myTimer.inputSeconds);
-    console.log(
-      `input hours: ${hours}\n` +
-        `input minutes: ${minutes}\n` +
-        `input seconds: ${seconds}\n` +
-        `\t-> total seconds: ${myTimer.leftSeconds}`
-    );
+    timer.inputTime = { hours, minutes, seconds };
+    displayTimeText(hours, minutes, seconds);
   }
 
-  function controlTimerState(event) {
-    if (myTimer.leftSeconds) {
-      displayTime(myTimer.leftSeconds, myTimer.inputSeconds);
-      switch (myTimer.state) {
-        case "Start":
-          startTimer(myTimer.inputSeconds);
-          myTimer.state = "Pause";
-          buttonStartPauseResume.textContent = "일시정지";
+  function controlTimerState() {
+    const status = timer._state.status;
+    if (status !== 'Finish') {
+      switch (status) {
+        case 'Start':
+          timer.start();
+          buttonStartPauseResume.textContent = '일시정지';
           break;
-        case "Pause":
-          pauseTimer(myTimer.id);
-          myTimer.state = "Resume";
-          buttonStartPauseResume.textContent = "재시작";
+        case 'Pause':
+          timer.pause();
+          buttonStartPauseResume.textContent = '계속';
           break;
-        case "Resume":
-          resumeTimer(myTimer.leftSeconds);
-          myTimer.state = "Pause";
-          buttonStartPauseResume.textContent = "일시정지";
-          break;
-        default:
+        case 'Resume':
+          timer.resume();
+          buttonStartPauseResume.textContent = '일시정지';
           break;
       }
     }
   }
 
-  formTime.addEventListener("submit", (event) => {
-    inputTime(event);
+  formTime.addEventListener('submit', (event) => {
+    event.preventDefault();
+    timer.reset();
+    setInputTime();
+    buttonStartPauseResume.textContent = '시작';
   });
 
-  buttonStartPauseResume.addEventListener("click", (event) => {
-    controlTimerState(event);
+  formTime.addEventListener('keyup', (event) => {
+    const target = event.target;
+    const maxLength = 2;
+    const currentLength = target.value.length;
+    if (target.tagName.toLowerCase() === 'input') {
+      if (
+        currentLength >= maxLength &&
+        target.nextElementSibling.tagName.toLowerCase() === 'input'
+      ) {
+        target.nextElementSibling.focus();
+      } else if (
+        currentLength === 0 &&
+        target.previousElementSibling.tagName.toLowerCase() === 'input'
+      ) {
+        target.previousElementSibling.focus();
+      }
+    }
   });
 
-  buttonReset.addEventListener("click", (e) => {
-    resetTimer();
+  buttonStartPauseResume.addEventListener('click', (event) => {
+    controlTimerState();
+  });
+
+  buttonReset.addEventListener('click', (e) => {
+    timer.reset();
+    buttonStartPauseResume.textContent = '시작';
   });
 }
+
+App();
